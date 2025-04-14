c1c4b6e1658836de86c48b6cc7d88d088c4d692d
Roberto Ramirez <Ramirez20@colum.edu>
2025-04-14 14:03:04 -0500

    Add project files.

---
+++ Weatherer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Weatherer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string APIKey = ""; // the secret
+        private string fileName;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            fileName = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&mode=xml&units=imperial&APPID=" + APIKey;
+        }
+
+        public string CurrentTemperature()
+        {
+            string description = "Cloudy";
+            string temperatureString = "60";
+            float temperature = 0;
+            XDocument xdoc = XDocument.Load(fileName);
+            var tempList = xdoc.Descendants()
+                          .Where(x => x.Name == "temperature" || x.Name == "weather");
+            foreach (XElement node in tempList)
+            {
+                if (node.Name == "temperature")
+                { temperatureString = node.Attribute("value").Value; }
+                if (float.TryParse(temperatureString, out float t))
+                { temperature = t; }
+                if (node.Name == "weather")
+                { description = node.Attribute("value").Value; }
+            }
+            return $"Temperature: {temperature} \n {description}";
+        }
+    }
+}
